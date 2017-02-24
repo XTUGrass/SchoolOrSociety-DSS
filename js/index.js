9854519179c93e2a132beaecbcccac420c98ba7d
crystal <972369976@qq.com>
2017-02-24 21:04:03 +0800

    index page accomplish

--- js/index.js
+++ js/index.js
@@ -1,17 +1,59 @@
 $(document).ready(function(){
-	//初始化背景图大小
-	var mastheadHeight=$('#assitImg').css('height');
-	mastheadHeight=parseInt(mastheadHeight);
-	mastheadHeight+=7;
-	console.log(mastheadHeight);
-	$('.masthead').css('height',mastheadHeight+'px');
-	//背景图随屏幕大小改变
+	var windowHeight=$(window).height();
+	$('#mastheadBg').css('height',windowHeight);
 	$(window).resize(function(){
-		var mastheadHeight=$('#assitImg').css('height');
-		mastheadHeight=parseInt(mastheadHeight);
-		mastheadHeight+=7;
-		$('.masthead').css('height',mastheadHeight);
-		console.log(typeof(mastheadHeight)+mastheadHeight);
+		var windowHeight=$(window).height();
+		$('#mastheadBg').css('height',windowHeight);
 	});
-	
+	//问号的动画
+
+	function rotateImg(){
+		if(angle<=357){
+			angle+=3;
+		}else{
+			angle=0;
+		}
+		$("#questionImg").find('img').rotate(angle);
+	}
+	var angle=0;
+	timer=setInterval(rotateImg,30);
+	$('#questionImg').find('img').on('mouseenter',function(){
+		clearInterval(timer);
+	});
+	$('#questionImg').find('img').on('mouseout',function(){
+		clearInterval(timer);
+		timer=setInterval(rotateImg,30);
+	});
+
+	//choice button
+	$('#choiceBtn').on('mouseover',function(){
+		$(this).css('opacity','1');
+	});
+	$('#choiceBtn').on('mouseout',function(){
+		$(this).css('opacity','0.7');
+	});
+
+	//systemBase brief
+	$('.brief').click(function(){
+		var baseIntro=$(this).closest('.col-xs-4').find('.baseIntro');
+		if(baseIntro.css('display').toLowerCase()=='none'){
+			$(this).rotate(45);
+		}else{
+			$(this).rotate(0);
+		}
+		baseIntro.slideToggle();	
+	});
+
+	//滚动条滚动到指定位置加载指定内容
+	$(window).scroll(function(){
+		if($(window).scrollTop()>=$(window).height()/3){
+			$('#systemBase1').show(1000);
+			$('.brief').fadeIn(2000);
+		}
+
+		if($(window).scrollTop()>=$(window).height()*2/3){
+			$('#obtainShow').slideDown(1000);
+		}	
+	});
+	console.log($(window).height());
 });
